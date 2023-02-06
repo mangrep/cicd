ba4fd5ded8401600d5a524d0658f6aaa7e5169e2
mangrep <manish@mavonic.com>
2023-02-06 14:31:05 +0530

    test 3

--- .github/scripts/grant-select-readonly-user.sh
+++ .github/scripts/grant-select-readonly-user.sh
@@ -2,7 +2,6 @@
 set -e
 
 export PGPASSWORD=$POSTGRES_PW
-COPY ./.github/scripts/grant-select-readonly-user.sh /scripts/grant-select-readonly-user.sh
 
 psql -h $POSTGRES_HOST -U "$POSTGRES_USER" "$POSTGRES_DB" -c "GRANT CONNECT ON DATABASE \""$POSTGRES_DB"\" TO \""$ADDITIONAL_DATABASE_USER_READONLY"\";"
 psql -h $POSTGRES_HOST -U "$POSTGRES_USER" "$POSTGRES_DB" -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \""$ADDITIONAL_DATABASE_USER_READONLY"\";"
